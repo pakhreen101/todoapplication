b8ee1d33119ec0df3fa5f44dd2dbaf70011b2909
pakhreen101 <pakhreenc101@gmail.com>
2025-09-05 11:20:58 +0900

    2nd commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Todo = ({todo,toggleTodo}) => {
+  const handleTodoClick=()=>{
+    toggleTodo(todo.id);
+  }
+  return (
+    <div>
+      <input type='checkbox' checked={todo.completed} readOnly onChange={handleTodoClick} />
+      {todo.name}
+      </div>
+  )
+}
+
+export default Todo
